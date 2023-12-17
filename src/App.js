417d6ab9d8db9a56aa1c12acf4d53d1f2607e1e1
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-16 22:19:24 -0400

    Component - Welcome [16/12/2023]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import            './App.css';
-import Nav   from './components/shared/Nav';
-import            'bootstrap/dist/css/bootstrap.min.css'
+import React   from 'react';
+import              './App.css';
+import Nav     from './components/shared/Nav';
+import              'bootstrap/dist/css/bootstrap.min.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="">
       <Nav />
+      <Welcome />
     </div>
   );
 }

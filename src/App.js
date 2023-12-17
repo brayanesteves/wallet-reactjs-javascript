f6b406d5c49ad0f6b6b991e8991f87a36897f7fb
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-16 22:41:01 -0400

    Component - Dashboard [16/12/2023]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React   from 'react';
-import              './App.css';
-import Nav     from './components/shared/Nav';
-import              'bootstrap/dist/css/bootstrap.min.css';
-import Welcome from './components/Welcome';
+import React                            from 'react';
+import Nav                              from './components/shared/Nav';
+import Welcome                          from './components/Welcome';
+import                                       './App.css';
+import                                       'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard                        from './components/dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="">
+    <BrowserRouter>
       <Nav />
-      <Welcome />
-    </div>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

79b9663427d515b14ec07ae238c2d6f89ccfab7f
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-17 15:29:17 -0400

    Redux - Store & rootReducer [17//12/2023]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React                            from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider }                     from 'react-redux';
 import Nav                              from './components/shared/Nav';
 import Welcome                          from './components/Welcome';
 import Dashboard                        from './components/dashboard/Dashboard';
@@ -7,18 +8,20 @@ import CreateWallet                     from './components/dashboard/operations/
 import NotFound                         from './components/shared/404';
 import                                       './App.css';
 import                                       'bootstrap/dist/css/bootstrap.min.css';
-
+import store                            from './utils/Store';
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create-wallet" element={<CreateWallet />} />
-        <Route path="/404" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-wallet" element={<CreateWallet />} />
+          <Route path="/404" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 

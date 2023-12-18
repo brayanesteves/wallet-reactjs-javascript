901fcc3e60d5efeaff528a105238c5d754dd6877
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-18 07:58:52 -0400

    Layer [actions] - projectActions, errorReducer, types [18/12/2023]

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { GET_ERRORS } from "./types";
 
 export const createWallet = (newWallet, history) => async dispath => {
     await axios.post(`http://localhost:8090/wallet`, newWallet).then((response) => {
         history.push('/dashboard');
     }).catch((error) => {
-
+        dispath({ type:GET_ERRORS, payload:error.response.data });
     });
 };

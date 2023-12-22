1315edf79f885505c2a1741c4a26998b91907b45
Brayan Esteves <brayan.esteves93@gmail.com>
2023-12-21 22:01:47 -0400

    Layer [actions] - projectActions [21/12/2023]

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,9 +1,17 @@
-import   axios        from "axios";
-import { GET_ERRORS } from "./types";
+import   axios                     from "axios";
+import { GET_ERRORS, GET_WALLETS } from "./types";
 
 export const createWallet = (newWallet, history) => async dispath => {
     await axios.post(`http://localhost:8090/wallet`, newWallet).then((response) => {
         history.push('/dashboard');
+    }).catch((error) => {
+        dispath({ type:GET_ERRORS, payload:error.response.data });
+    });
+};
+
+export const getWallets = (newWallet, history) => async dispath => {
+    await axios.post(`http://localhost:8090/wallet/find-all-order-by-priority`, newWallet).then((response) => {
+        dispath({ type:GET_WALLETS, payload:response.data });
     }).catch((error) => {
         dispath({ type:GET_ERRORS, payload:error.response.data });
     });
